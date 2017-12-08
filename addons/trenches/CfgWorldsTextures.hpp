d126662471305cd7881c48e2290f324bf53217fa
Salbei <karsus3@web.de>
2017-12-08 10:40:02 +0100

    More fixes

--- addons/trenches/CfgWorldsTextures.hpp
+++ addons/trenches/CfgWorldsTextures.hpp
@@ -13,21 +13,21 @@ class CfgWorldsTextures {
     class Bootcamp_ACR {
         surfaceTextureBasePath = "ca\Bootcamp_ACR\data\";
         filePrefix = "detail_co.paa";
-        camouflageObjects[] = {"ca\plants_E2\clutter\c_summer_flowers.p3d"};
+        camouflageObjects[] = {"ca\plants2\clutter\c_GrassBunch.p3d"};
     };
 
     // Bystrica
     class Woodland_ACR {
         surfaceTextureBasePath = "ca\Bohemia\data\";
         filePrefix = "detail_co.paa";
-        camouflageObjects[] = {"ca\plants2\clutter\c_fern.p3d"};
+        camouflageObjects[] = {"ca\plants2\clutter\c_weed3.p3d"};
     };
 
     //Chernarus
     class Chernarus {
         surfaceTextureBasePath = "ca\CHERNARUS\data\";
         filePrefix = "detail_co.paa";
-        camouflageObjects[] = {"CUP\Terrains\cup_terrains_worlds\Clutter\c_GrassCrookedGreen.p3d"};
+        camouflageObjects[] = {"ca\plants2\clutter\c_weed3.p3d"};
     };
 
     //Celle
@@ -37,7 +37,14 @@ class CfgWorldsTextures {
 
     //Chernarus Summer
 	  class Chernarus_Summer: Chernarus {
-        camouflageObjects[] = {};
+        camouflageObjects[] = {"ca\plants2\clutter\c_weed3.p3d"};
+    };
+
+    //Desert
+    class Desert_E {
+        surfaceTextureBasePath = "ca\Desert_E\data\";
+        filePrefix = "co.paa";
+        camouflageObjects[] = {"ca\plants_E\Clutter\c_Brush_Hard_EP1.p3d"};
     };
 
     //Lythium
@@ -52,23 +59,16 @@ class CfgWorldsTextures {
         camouflageObjects[] = {"A3\plants_f\Clutter\c_Thistle_Thorn_Brown.p3d"};
     };
 
-  //Porto
-	class Porto {
-    		surfaceTextureBasePath = "ca\desert2\data\";
-    		filePrefix = "detail_co.paa";
-        camouflageObjects[] = {};
-	};
-
-    //Desert
-    class Desert_E {
-        surfaceTextureBasePath = "ca\Desert_E\data\";
-        filePrefix = "co.paa";
-        camouflageObjects[] = {"ca\plants_E\Clutter\c_GrassDesert_GroupSoft_EP1.p3d"};
-    };
-
     //Rahmadi
     class Intro: Chernarus {
          camouflageObjects[] = {"ca\plants\clutter_grass_flowers.p3d"};
+    };
+
+    //Porto
+    class Porto {
+          surfaceTextureBasePath = "ca\desert2\data\";
+          filePrefix = ".paa";
+          camouflageObjects[] = {};
     };
 
     //Prei Khmaoch Luong
@@ -82,14 +82,96 @@ class CfgWorldsTextures {
     class ProvingGrounds_PMC {
         surfaceTextureBasePath = "ca\provinggrounds_pmc\Data\";
         filePrefix = "co.paa";
-        camouflageObjects[] = {"ca\plants_pmc\Clutter\c_GrassTall_pmc.p3d"};
+        camouflageObjects[] = {};
     };
 
   //Sahrani
 	class Sara {
       surfaceTextureBasePath = "ca\sara\Data\";
-      filePrefix = "detail_co.paa";
+      filePrefix = "_detail_co.paa";
       camouflageObjects[] = {"ca\hotfix\ClutterFix_Grass_Long.p3d"};
+      class Surfaces {
+       class blato_detail_nohq {
+         texturePath = "ca\sara\Data\blato_detail_nohq.paa";
+       };
+       class blato_mco {
+         texturePath = "ca\sara\Data\blato_mco.paa";
+       };
+       class lesjeh_detail_nohq {
+         texturePath = "ca\sara\Data\lesjeh_detail_nohq.paa";
+       };
+       class lesjeh_mco {
+         texturePath = "ca\sara\Data\lesjeh_mco.paa";
+       };
+       class leslist_detail_nohq {
+         texturePath = "ca\sara\Data\leslist_detail_nohq.paa";
+       };
+       class leslist_mco {
+         texturePath = "ca\sara\Data\leslist_mco.paa";
+       };
+       class mesto_detail_nohq {
+         texturePath = "ca\sara\Data\mesto_detail_nohq.paa";
+       };
+       class mesto_mco {
+         texturePath = "ca\sara\Data\mesto_mco.paa";
+       };
+       class mesto2_detail_nohq {
+         texturePath = "ca\sara\Data\mesto2_detail_nohq.paa";
+       };
+       class mesto2_mco {
+         texturePath = "ca\sara\Data\mesto2_mco.paa";
+       };
+       class pisek_detail_nohq {
+         texturePath = "ca\sara\Data\pisek_detail_nohq.paa";
+       };
+       class pisek_mco {
+         texturePath = "ca\sara\Data\pisek_mco.paa";
+       };
+       class pisekplaz_detail_nohq {
+         texturePath = "ca\sara\Data\pisekplaz_detail_nohq.paa";
+       };
+       class pisekplaz_mco {
+         texturePath = "ca\sara\Data\pisekplaz_mco.paa";
+       };
+       class pisekpoust_detail_nohq {
+         texturePath = "ca\sara\Data\pisekpoust_detail_nohq.paa";
+       };
+       class pisekpoust_mco {
+         texturePath = "ca\sara\Data\pisekpoust_mco.paa";
+       };
+       class pole1_detail_nohq {
+         texturePath = "ca\sara\Data\pole1_detail_nohq.paa";
+       };
+       class pole1_mco {
+         texturePath = "ca\sara\Data\pole1_mco.paa";
+       };
+       class sara_a3_middle_mco {
+         texturePath = "ca\sara\Data\sara_a3_middle_mco.paa";
+       };
+       class skalaj_detail_nohq {
+         texturePath = "ca\sara\Data\skalaj_detail_nohq.paa";
+       };
+       class skalaj_mco {
+         texturePath = "ca\sara\Data\skalaj_mco.paa";
+       };
+       class skalas_detail_nohq{
+         texturePath = "ca\sara\Data\skalas_detail_nohq.paa";
+       };
+       class skalas_mco {
+         texturePath = "ca\sara\Data\skalas_mco.paa";
+       };
+       class trava_detail_nohq {
+         texturePath = "ca\sara\Data\trava_detail_nohq.paa";
+       };
+       class trava_mco {
+         texturePath = "ca\sara\Data\trava_mco.paa";
+       };
+       class travajih_detail_nohq {
+         texturePath = "ca\sara\Data\travajih_detail_nohq.paa";
+       };
+       class travajih_mco {
+         texturePath = "ca\sara\Data\travajih_mco.paa";
+       };
 	};
 
   //Shapur
@@ -102,45 +184,86 @@ class CfgWorldsTextures {
   //Southern Sahrani
   class SaraLite {
      surfaceTextureBasePath = "ca\saralite\Data\";
-     filePrefix = ".paa";
-     camouflageObjects[] = {};
+     filePrefix = "_detail_co.paa";
+     camouflageObjects[] = {"ca\plants\clutter_grass_general.p3d"};
+     class Surfaces {
+ 			class leslist_detail_nohq {
+ 				texturePath = "ca\saralite\Data\leslist_detail_nohq.paa";
+ 			};
+      class leslist_mco {
+ 				texturePath = "ca\saralite\Data\leslist_mco.paa";
+ 			};
+      class mesto_detail_nohq {
+ 				texturePath = "ca\saralite\Data\mesto_detail_nohq.paa";
+ 			};
+      class mesto_mco {
+        texturePath = "ca\saralite\Data\mesto_mco.paa";
+      };
+      class pisek_detail_nohq {
+        texturePath = "ca\saralite\Data\pisek_detail_nohq.paa";
+      };
+      class pisek_mco {
+        texturePath = "ca\saralite\Data\pisek_mco.paa";
+      };
+      class pisekpoust_detail_nohq {
+        texturePath = "ca\saralite\Data\pisekpoust_detail_nohq.paa";
+      };
+      class pisekpoust_mco {
+        texturePath = "ca\saralite\Data\pisekpoust_mco.paa";
+      };
+      class saralite_a3_middle_mco {
+        texturePath = "ca\saralite\Data\saralite_a3_middle_mco.paa";
+      };
+      class skalaj_detail_nohq {
+        texturePath = "ca\saralite\Data\skalaj_detail_nohq.paa";
+      };
+      class skalaj_mco {
+        texturePath = "ca\saralite\Data\skalaj_mco.paa";
+      };
+      class travajih_detail_nohq {
+        texturePath = "ca\saralite\Data\travajih_detail_nohq.paa";
+      };
+      class travajih_mco {
+        texturePath = "ca\saralite\Data\travajih_mco.paa";
+      };
+ 		};
   };
 
   //Stratis
   class Stratis: Altis {
-     camouflageObjects[] = {};
+        camouflageObjects[] = {"A3\plants_f\Clutter\c_Thistle_Thorn_Brown.p3d"};
   };
 
   //Takistan
 	class Takistan {
 		 surfaceTextureBasePath = "ca\takistan\Data\";
 		 filePrefix = "co.paa";
-     camouflageObjects[] = {};
+     camouflageObjects[] = {"ca\plants_E\Clutter\c_Brush_Soft_EP1.p3d"};
 	};
 
   //Takistan Mountains
 	class Mountains_ACR {
 		 surfaceTextureBasePath = "ca\afghan\Data\";
      filePrefix = "co.paa";
-     camouflageObjects[] = {};
+     camouflageObjects[] = {"ca\plants_E\Clutter\c_GrassGreen_EP1.p3d"};
 	};
 
   //Tanoa
   class Tanoa : Altis{
      surfaceTextureBasePath = "A3\Map_Data_Exp\";
-     camouflageObjects[] = {};
+     camouflageObjects[] = {"A3\Vegetation_F_Exp\Clutter\Grass\c_GrassBunch_HI.p3d"};
   };
 
   //United Sahrani
   class Sara_dbe1 : Sara {
-     camouflageObjects[] = {};
+     camouflageObjects[] = {"ca\plants\clutter_grass_general.p3d"};
   };
 
   //Utes
 	class utes {
 		surfaceTextureBasePath = "ca\utes\Data\";
-		filePrefix = "detail_co.paa";
-    camouflageObjects[] = {};
+		filePrefix = ".paa";
+    camouflageObjects[] = {"CUP\Terrains\cup_terrains_worlds\Clutter\c_GrassCrookedForest.p3d"};
 		class Surfaces {
 			class Default {
 				texturePath = "a3\map_data\gdt_beach_co.paa";
@@ -152,5 +275,6 @@ class CfgWorldsTextures {
 	class Zargabad {
 		surfaceTextureBasePath = "ca\zargabad\Data\";
 		filePrefix = "co.paa";
+    camouflageObjects[] = {"ca\plants_E\Clutter\c_Brush_Soft_EP1.p3d"};
 	};
 };
