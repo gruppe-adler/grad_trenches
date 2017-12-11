e214c02b1125012251bb4d1f8787b8f3c192ce69
Christian Klemm <chris579@web.de>
2017-12-11 13:35:00 +0100

    moved apl models to separate folder

--- addons/assets/CfgVehicles.hpp
+++ addons/assets/CfgVehicles.hpp
@@ -3,20 +3,20 @@ class CBA_Extended_EventHandlers;
 class CfgVehicles {
     class BagFence_base_F;
     class ACE_envelope_small: BagFence_base_F {
-        model = QPATHTOF(data\ace_envelope_small4.p3d);
+        model = QPATHTOF(apl\ace_envelope_small4.p3d);
 		hiddenSelections[] = {"velka"};
     };
     class ACE_envelope_big: BagFence_base_F {
-        model = QPATHTOF(data\ace_envelope_big4.p3d);
+        model = QPATHTOF(apl\ace_envelope_big4.p3d);
 		hiddenSelections[] = {"velka"};
     };
 
 	class ACE_envelope_small_noGeo: BagFence_base_F {
-        model = QPATHTOF(data\ace_envelope_small4_nogeo.p3d);
+        model = QPATHTOF(apl\ace_envelope_small4_nogeo.p3d);
 		hiddenSelections[] = {"velka"};
     };
     class ACE_envelope_big_noGeo: BagFence_base_F {
-        model = QPATHTOF(data\ace_envelope_big4_nogeo.p3d);
+        model = QPATHTOF(apl\ace_envelope_big4_nogeo.p3d);
 		hiddenSelections[] = {"velka"};
     };
 };
