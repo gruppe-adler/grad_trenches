2f001b8dd7009b7d65460027c14fe4abb24c6b7c
Christian Klemm <chris579@web.de>
2017-12-11 14:39:58 +0100

    config adjusts

--- addons/assets/CfgVehicles.hpp
+++ addons/assets/CfgVehicles.hpp
@@ -10,6 +10,10 @@ class CfgVehicles {
         model = QPATHTOF(apl\ace_envelope_big4.p3d);
 		hiddenSelections[] = {"velka"};
     };
+    class GRAD_envelope_gigant: BagFence_base_F {
+        model = QPATHTOF(data\trench_gigant.p3d);
+        hiddenSelections[] = {"velka"};
+    };
 
 	class ACE_envelope_small_noGeo: BagFence_base_F {
         model = QPATHTOF(apl\ace_envelope_small4_nogeo.p3d);
@@ -19,12 +23,8 @@ class CfgVehicles {
         model = QPATHTOF(apl\ace_envelope_big4_nogeo.p3d);
 		hiddenSelections[] = {"velka"};
     };
-
-    class GRAD_envelope_gigant: BagFence_base_F {
-        model = QPATHTOF(data\trench_gigant.p3d);
-        hiddenSelections[] = {"velka"};
-    };
     class GRAD_envelope_gigant_noGeo: BagFence_base_F {
+        scope = 1;
         model = QPATHTOF(data\trench_gigant_nogeo.p3d);
         hiddenSelections[] = {"velka"};
     };
