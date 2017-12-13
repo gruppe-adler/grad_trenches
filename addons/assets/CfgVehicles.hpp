92fe3244d44362b1118ba3875b6501675c6e12a3
Christian Klemm <chris579@web.de>
2017-12-13 15:56:28 +0100

    tabs to spaces

--- addons/assets/CfgVehicles.hpp
+++ addons/assets/CfgVehicles.hpp
@@ -4,24 +4,24 @@ class CfgVehicles {
     class BagFence_base_F;
     class ACE_envelope_small: BagFence_base_F {
         model = QPATHTOF(apl\ace_envelope_small4.p3d);
-		hiddenSelections[] = {"velka"};
+        hiddenSelections[] = {"velka"};
     };
     class ACE_envelope_big: BagFence_base_F {
         model = QPATHTOF(apl\ace_envelope_big4.p3d);
-		hiddenSelections[] = {"velka"};
+        hiddenSelections[] = {"velka"};
     };
     class GRAD_envelope_gigant: BagFence_base_F {
         model = QPATHTOF(data\trench_gigant.p3d);
         hiddenSelections[] = {"velka"};
     };
 
-	class ACE_envelope_small_noGeo: BagFence_base_F {
+    class ACE_envelope_small_noGeo: BagFence_base_F {
         model = QPATHTOF(apl\ace_envelope_small4_nogeo.p3d);
-		hiddenSelections[] = {"velka"};
+        hiddenSelections[] = {"velka"};
     };
     class ACE_envelope_big_noGeo: BagFence_base_F {
         model = QPATHTOF(apl\ace_envelope_big4_nogeo.p3d);
-		hiddenSelections[] = {"velka"};
+        hiddenSelections[] = {"velka"};
     };
     class GRAD_envelope_gigant_noGeo: BagFence_base_F {
         scope = 1;
