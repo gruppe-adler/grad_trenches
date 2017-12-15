5e4be6c45b6f8cc6b069609d515fcd564238f813
Salbei <karsus3@web.de>
2017-12-15 18:00:27 +0100

    Exchanged the old models with the new

--- addons/assets/CfgVehicles.hpp
+++ addons/assets/CfgVehicles.hpp
@@ -3,18 +3,10 @@ class CBA_Extended_EventHandlers;
 class CfgVehicles {
     class BagFence_base_F;
     class ACE_envelope_small: BagFence_base_F {
-        model = QPATHTOF(apl\ace_envelope_small4.p3d);
-        hiddenSelections[] = {"velka"};
-    };
-    class ACE_envelope_big: BagFence_base_F {
-        model = QPATHTOF(apl\ace_envelope_big4.p3d);
-        hiddenSelections[] = {"velka"};
-    };
-    class GRAD_envelope_small: BagFence_base_F {
         model = QPATHTOF(data\trench_small.p3d);
         hiddenSelections[] = {"velka"};
     };
-    class GRAD_envelope_big: BagFence_base_F {
+    class ACE_envelope_big: BagFence_base_F {
         model = QPATHTOF(data\trench_big.p3d);
         hiddenSelections[] = {"velka"};
     };
@@ -27,20 +19,11 @@ class CfgVehicles {
         hiddenSelections[] = {"velka"};
     };
 
-
     class ACE_envelope_small_noGeo: BagFence_base_F {
-        model = QPATHTOF(apl\ace_envelope_small4_nogeo.p3d);
-        hiddenSelections[] = {"velka"};
-    };
-    class ACE_envelope_big_noGeo: BagFence_base_F {
-        model = QPATHTOF(apl\ace_envelope_big4_nogeo.p3d);
-        hiddenSelections[] = {"velka"};
-    };
-    class GRAD_envelope_small_noGeo: BagFence_base_F {
         model = QPATHTOF(data\trench_small_nogeo.p3d);
         hiddenSelections[] = {"velka"};
     };
-    class GRAD_envelope_big_noGeo: BagFence_base_F {
+    class ACE_envelope_big_noGeo: BagFence_base_F {
         model = QPATHTOF(data\trench_big_nogeo.p3d);
         hiddenSelections[] = {"velka"};
     };
