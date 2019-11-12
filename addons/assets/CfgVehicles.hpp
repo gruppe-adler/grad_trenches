0bcc2e0fa92815ed1941f2c40be328cf291686c7
Salbei <salbei@gruppe-adler.de>
2019-11-12 06:45:10 +0100

    added pictures & lowered trenches

--- addons/assets/CfgVehicles.hpp
+++ addons/assets/CfgVehicles.hpp
@@ -5,21 +5,26 @@ class CfgVehicles {
     class GRAD_envelope_short: BagFence_base_F {
         model = QPATHTOF(data\trench_short.p3d);
         hiddenSelections[] = {"velka"};
+        editorPreview = QPATHTOF(data\trench_short.paa);
     };
     class ACE_envelope_small: BagFence_base_F {
         model = QPATHTOF(data\trench_small.p3d);
         hiddenSelections[] = {"velka"};
+        editorPreview = QPATHTOF(data\trench_small.paa);
     };
     class ACE_envelope_big: BagFence_base_F {
         model = QPATHTOF(data\trench_big.p3d);
         hiddenSelections[] = {"velka"};
+        editorPreview = QPATHTOF(data\trench_big.paa);
     };
     class GRAD_envelope_giant: BagFence_base_F {
         model = QPATHTOF(data\trench_giant.p3d);
         hiddenSelections[] = {"velka"};
+        editorPreview = QPATHTOF(data\trench_giant.paa);
     };
     class GRAD_envelope_vehicle: BagFence_base_F {
         model = QPATHTOF(data\trench_vehicle.p3d);
         hiddenSelections[] = {"velka"};
+        editorPreview = QPATHTOF(data\trench_vehicle.paa);
     };
 };
