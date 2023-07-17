afafd9288540c8f683c5c20da5a0b1b7fa7d2210
Salbei <salbei@gruppe-adler.de>
2023-07-17 23:12:07 +0200

    fixed missing gm vehicle variants

--- addons/functions/CfgDigVehicles.hpp
+++ addons/functions/CfgDigVehicles.hpp
@@ -27,6 +27,19 @@ class CfgDigVehicles {
         distanceToTrench = 3;
     };
 
+    class gm_ge_army_bpz2a0_base : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_des : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_oli : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_ols : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_olw : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_trp : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_un : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_wdl : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_wds : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_win : gm_ge_army_bpz2a0 {};
+    class gm_ge_army_bpz2a0_wiw : gm_ge_army_bpz2a0 {};
+
+
     class gm_dk_army_bpz2a0 {
         type = "source";
         animation = "dozer_blade_elev_source";
@@ -35,4 +48,11 @@ class CfgDigVehicles {
         plowLowered = .7;
         distanceToTrench = 3;
     };
+
+    class gm_dk_army_bpz2a0_base : gm_dk_army_bpz2a0 {};
+    class gm_dk_army_bpz2a0_des : gm_dk_army_bpz2a0 {};
+    class gm_dk_army_bpz2a0_tan : gm_dk_army_bpz2a0 {};
+    class gm_dk_army_bpz2a0_un : gm_dk_army_bpz2a0 {};
+    class gm_dk_army_bpz2a0_wdl : gm_dk_army_bpz2a0 {};
+    class gm_dk_army_bpz2a0_win : gm_dk_army_bpz2a0 {};
 };
